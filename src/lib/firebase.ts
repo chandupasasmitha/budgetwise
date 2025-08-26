aa36db28637475707407fc24635838979b97a151
chandupa sasmitha <chandupasasmitha@gmail.com>
2025-08-26 10:07:13 +0000

    firebase : error auth configuration not found

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,12 +2,12 @@ import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-    projectId: "budgetwise-41m86",
-    appId: "1:373648485369:web:3f0addbc5bac8a898f2b28",
-    storageBucket: "budgetwise-41m86.firebasestorage.app",
     apiKey: "AIzaSyA3iLcMZigjY_fkYiS87RwpjZS3sW2DCUE",
     authDomain: "budgetwise-41m86.firebaseapp.com",
-    messagingSenderId: "373648485369"
+    projectId: "budgetwise-41m86",
+    storageBucket: "budgetwise-41m86.firebasestorage.app",
+    messagingSenderId: "373648485369",
+    appId: "1:373648485369:web:3f0addbc5bac8a898f2b28"
 };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
