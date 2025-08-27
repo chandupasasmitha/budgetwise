dc53f0c3806475843279233b58f26f53bb8bf774
chandupasasmitha <chandupasasmitha@gmail.com>
2025-08-27 18:51:15 +0530

    firebase auth connect

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,11 +1,12 @@
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
     apiKey: "AIzaSyA3iLcMZigjY_fkYiS87RwpjZS3sW2DCUE",
     authDomain: "budgetwise-41m86.firebaseapp.com",
     projectId: "budgetwise-41m86",
-    storageBucket: "budgetwise-41m86.firebasestorage.app",
+    storageBucket: "budgetwise-41m86.appspot.com",
     messagingSenderId: "373648485369",
     appId: "1:373648485369:web:3f0addbc5bac8a898f2b28"
 };
@@ -14,3 +15,4 @@ const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 const auth = getAuth(app);
 
 export { app, auth };
+export const db = getFirestore(app);
