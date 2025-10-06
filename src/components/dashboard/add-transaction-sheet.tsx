0af5f0111deefcd440b54214cf8f4eac50df5942
chandupa sasmitha <chandupasasmitha@gmail.com>
2025-10-06 14:39:04 +0000

    in mobile view add transaction showing in + mark. change that to add tra

--- src/components/dashboard/add-transaction-sheet.tsx
+++ src/components/dashboard/add-transaction-sheet.tsx
@@ -238,7 +238,7 @@ function AddTransactionSheet({ bookId, onTransactionAdded }: AddTransactionSheet
         <SheetTrigger asChild>
           <Button size="sm" className="h-8 gap-1">
             <PlusCircle className="h-3.5 w-3.5" />
-            <span className="sr-only sm:not-sr-only sm:whitespace-nowrap">
+            <span className="whitespace-nowrap">
               Add Transaction
             </span>
           </Button>
